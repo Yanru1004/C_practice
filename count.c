e59af48c4240e0164428f12c3d03f126a33a74fc
Yanru1004 <54731761+Yanru1004@users.noreply.github.com>
2025-02-12 23:16:31 +0800

    Add files via upload

---
+++ count.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    int count = printf("T\n");
+    printf("%d\n", count);
+
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    int sorce = 10;
+    printf("Sorce:%d",sorce*2);
+    
+    return 0;
+
+}
+
