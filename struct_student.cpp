db79d2b12461126be897a6c9e498418f788897a3
devil1004 <devil1004@livemail.tw>
2025-03-29 14:10:39 +0800

    20250329_up

---
+++ struct_student.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+struct student{
+    char name[50];
+    int  no;
+    int  math;
+    int  eng;
+    int  chi;
+}
+
+student s1,s2;
+
+void main(){
+    return ;
+}
