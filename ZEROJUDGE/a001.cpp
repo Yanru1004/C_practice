a4cef5ceb6c465ffa83b663cc4115ce1d2e867c4
Yanru1004 <devil1004@livemail.tw>
2025-06-20 23:34:02 +0800

    20250620_practice

---
+++ ZEROJUDGE/a001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    char str[50]; //宣告字元陣列a 長度50
+
+    cin >> str;   //讀入資訊到變數 str
+    cout <<"hello, "<< str;  //印出變數 str 內容
+    
+    return 0;
+}
+//2025.06.20 AC
---
+++ ZEROJUDGE/a002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b; //宣告 a,b 變數為int
+
+    cin >> a >>b; //讀入 a,b 值
+    cout << a+b;  //輸出加總
+
+    return 0;
+
+}
+//2025.06.20 AC
