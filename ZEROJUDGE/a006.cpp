a373485b2f0ab1a6097ed8ba556a6b6245b73abc
Yanru1004 <devil1004@livemail.tw>
2025-06-22 23:44:57 +0800

    2050622_practice

--- ZEROJUDGE/a006.cpp
+++ ZEROJUDGE/a006.cpp
@@ -5,10 +5,30 @@ using namespace std;
 
 int main(){
 
-    int a,b,c
+    int a,b,c;   //宣告三項係數
+    float check; //宣告檢查變數
 
-    cin >>a >>b >>c;
+    cin >>a >>b >>c; //輸入三項係數
 
-    if 
+    check = pow(b,2)- 4*a*c; //b**2 - 4ac
+
+    if (check < 0)   //b**2-4ac小於0 無實根
+        cout << "No real root";
     
+    else if (check == 0)  //b**2-4ac等於0 重根
+        cout << "Two same roots x=" << -b / (2*a);
+    
+    else {
+        float x1,x2; //b**-4ac 大於0 有2個實樹根
+        //計算 x1, x2 解
+        x1 = (-b+sqrt(check))/2*a;
+        x2 = (-b-sqrt(check))/2*a;
+        //比較 x1 , x2 大小
+        if (x1>x2)
+            cout << "Two different roots x1="<< x1<< " , x2="<< x2;
+        else
+            cout << "Two different roots x1="<< x2<< " , x2="<< x1;
+    } 
+    return 0;
 }
+//2025.06.22 AC
