bab07401b90e40c1d55e5f57ff9f7391efa7b5d9
Yanru1004 <devil1004@livemail.tw>
2025-07-05 23:15:44 +0800

    recoding_a010.cpp

--- ZEROJUDGE/a010.cpp
+++ ZEROJUDGE/a010.cpp
@@ -1,66 +1,38 @@
 //a010.因數分解
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(){
 
-    //輸入欲計算的數字
-    int n; 
+    //輸入n
+    int n,time; 
+    bool first = true; //第一次不輸出" * "的flag
+
     cin >> n;
-    
-    //宣告儲存質數陣列(pn)
-    vector<int> pn;
 
-    //-----建立質數表-----
-    for (int i=2;i<=n;i++){
-        bool isPrime = true;
+    for (int i=2;i<=n;(i==2?i++:i+=2)){  //i增量先2+1加到3，之後皆+2僅驗證奇數 i=2 i+1 i!=2 i+2
+        time = 0;
+        while (n%i ==0){ //多次除以i直到不能整除
+            n /= i;  //n = n / i
+            time ++; //記錄次方數    
+        }
         
-        if (pn.empty()){ //如果pn陣列為空放入i(用於放入2)
-            pn.push_back(i);            
-            continue;}
+        //輸出
+        if (first == false && time != 0) //非第一次輸出且有次方數(i是n的因數)
+            cout << " * ";
 
-        else{
-            //pn陣列元素與i相除能整除者非質數，以brrak跳出。
-            for (int j=0;j<pn.size() && pn[j]*pn[j] <= i;j++){
-                if (i % pn[j] == 0){
-                    isPrime = false; //註記為非質數
-                    break; }
-                }
-            //順利脫離迴圈沒被中斷，isPrime改為false。
-            //為質數放入pn陣列
-            if (isPrime == true){
-                pn.push_back(i);}
-            
-            }           
+        if (time == 1){ //如果次方數是1，不輸出次方數。
+            cout << i;
+            first = false; //輸出過第一次輸出flag改為false
         }
-    
-    //進行因數分解
-    
-    //遍歷質數表
-    bool first = true;
-    for (int i=0;i<pn.size();i++){
-        
-        int t = 0;
-        while (n % pn[i] == 0){
-            n /= pn[i];
-            t ++;
+        else if (time >1){ //如果次方數超過1，輸出因數及次方數。
+            cout << i << "^" << time;
+            first = false; //輸出過第一次輸出flag改為false
         }
-        //如果time > 0，進行輸出
-        if (t >0){
-            (first ? cout <<"":cout<<" * ");
-            if (t>1)
-                cout << pn[i] << "^" <<t;
-            else
-                cout << pn[i];
-            first = false;
-        }
-
 
     }
-
     return 0;
     }
-  
+ //2025.07.05 AC 
 
